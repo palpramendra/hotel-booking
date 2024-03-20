7b87771a8704814a53082665089e09433cdac7b3
palpramendra <pal.p90315@gmail.com>
2024-03-20 19:08:34 +0545

    about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -7,7 +7,7 @@ function About() {
         <div className="container">
             <div className="row">
                 <div className="col-lg-5">
-                <h4 className="our-room">OUR ROOMS</h4>
+                <h4 className="our-room">About</h4>
                 <p className='about'>The passage experienced a surge in popularity during the 1960s when Letraset used it on their dry-transfer sheets, and again during the 90s as desktop publishers bundled the text with their software. Today it's seen all around the web; on templates, websites, and stock designs. Use our generator to get your own, or read on for the authoritative history of lorem ipsum.</p>
                 <button className='btn'>Read More</button>
                 </div>
